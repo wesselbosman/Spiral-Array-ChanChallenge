d873813d3d37cabe6a3e337b61a8253a87e0091c
Wessel Bosman <wessel.bosman@hotmail.com>
2018-05-23 23:36:04 +0100

    Add console app to view display

---
+++ SpiralDisplay/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core;
+
+namespace SpiralDisplay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var spiralDisplay = new SpiralArray(10, 10);
+            Console.Write(spiralDisplay.ToString());
+            Console.ReadLine();
+        }
+    }
+}
